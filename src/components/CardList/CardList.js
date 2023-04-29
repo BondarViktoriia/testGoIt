6698ce81ee541fcecfbffca1f1096f432d8914eb
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-29 19:07:26 +0300

    add card to local storage + button hide

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,27 +1,42 @@
 import Card from 'components/Card/Card';
 import { CardListBox,LoadMore,LoadMoreContainer } from './CardList.styled';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 const CardList = ({ users }) => {
-    const [visibleCards, setVisibleCards] = useState(4);
+    // const [visibleCards, setVisibleCards] = useState(4);
+
+      const [visibleCards, setVisibleCards] = useState(
+    Number(localStorage.getItem('visibleCards')) || 4
+  );
+
+  useEffect(() => {
+    localStorage.setItem('visibleCards', visibleCards);
+  }, [visibleCards]);
     
   const loadMore = () => {
     setVisibleCards(visibleCards + 4);
+    };
+     const hidden = () => {
+    setVisibleCards(visibleCards - 4);
   };
     
   return (
     <CardListBox>
       {users.slice(0, visibleCards).map(user => (
-        //   <Card key={user.id} id={user.id} avatar={user.avatar} tweets={user.tweets} followers={user.followers } />
                     <Card key={user.id} user={user} />
 
       ))}
-          {visibleCards < users.length && (
+          {visibleCards < users.length &&  (
               <LoadMoreContainer>
                           <LoadMore onClick={loadMore}>Load More</LoadMore>
-
               </LoadMoreContainer>
-      )}    
+          )}    
+                    {visibleCards > 4 &&  (
+              <LoadMoreContainer>
+<LoadMore onClick={hidden}>Hide</LoadMore>
+              </LoadMoreContainer>
+          )}    
+          
     </CardListBox>
   );
 };
